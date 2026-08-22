23df9685822326bed465b2d1c7e8111963f6f26e
salah295 <68158409+salah295@users.noreply.github.com>
2026-08-22 19:31:50 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./goals.html', './src/**/*.{js,jsx}', './GoalPlannerDashboard.jsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
